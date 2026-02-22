72a14cef4805a7de5bc70b096574c84361ccbbd2
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-02-22 12:24:31 +0530

    Add Testbench file for Half-Adder

--- Half-Adder/tb/tb_halfadder.v
+++ Half-Adder/tb/tb_halfadder.v
@@ -1,1 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.01.2026 20:38:21
+// Design Name: 
+// Module Name: tb_halfadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module tb_halfadder();
+reg a,b;
+wire sum,carry;
+halfadder d0(a,b,sum,carry);
+initial begin
+#10 a = 0; b = 0;
+#10 a = 0; b = 1;
+#10 a = 1; b = 0;
+#10 a = 1; b = 1;
+#10 $finish;
+end
+endmodule
