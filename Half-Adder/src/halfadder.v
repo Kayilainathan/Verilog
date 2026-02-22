b5cd82845ba2fb494e0892c710a93622bf22efd0
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-02-22 12:20:19 +0530

    Add halfadder.v file

--- Half-Adder/src/halfadder.v
+++ Half-Adder/src/halfadder.v
@@ -1,1 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.01.2026 20:06:13
+// Design Name: 
+// Module Name: halfadder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+//half adder
+module halfadder(
+input a,
+input b,
+output sum,
+output carry
+    );
+    assign sum = a ^ b;
+    assign carry = a & b;
+endmodule
