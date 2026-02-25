61701548b89d12b8dafa3c201cb5fb00540b691e
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-02-25 22:50:15 +0530

    Add Half Subtractor Source

--- Half Subtractor/src/halfsub.v
+++ Half Subtractor/src/halfsub.v
@@ -1,1 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.01.2026 22:28:48
+// Design Name: 
+// Module Name: halfsub
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module halfsub(
+input a,
+input b,
+output diff,
+output borrow
+    );
+    assign diff = a ^ b;
+    assign borrow = ~a & b;
+endmodule
