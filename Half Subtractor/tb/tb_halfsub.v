64e324488c8a18a2906730dc1be9ff0f7669bc95
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-02-25 22:51:49 +0530

    Add Testbench for Half Subtractor

--- Half Subtractor/tb/tb_halfsub.v
+++ Half Subtractor/tb/tb_halfsub.v
@@ -1,1 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08.01.2026 22:31:27
+// Design Name: 
+// Module Name: tb_halfsub
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module tb_halfsub();
+reg a,b;
+wire diff,borrow;
+halfsub d0 (a,b,diff,borrow);
+initial begin
+#10 a = 0; b = 0;
+#10 a = 0; b = 1;
+#10 a = 1; b = 0;
+#10 a = 1; b = 1;
+end
+endmodule
