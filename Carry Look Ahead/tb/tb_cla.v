65ee52f11b304ef7b2b6e5d82fe17cf696f5c38c
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-03-03 22:32:20 +0530

    Add Testbench of the Carry Look Ahead

--- Carry Look Ahead/tb/tb_cla.v
+++ Carry Look Ahead/tb/tb_cla.v
@@ -1,1 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 14.01.2026 20:59:36
+// Design Name: 
+// Module Name: tb_cla
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module tb_cla();
+reg [3:0] a,b;
+reg cin;
+wire [3:0] sum;
+wire cout;
+integer i;
+cla jk (a,b,cin,sum,cout);
+initial begin
+ cin = 0;
+  for(i=0;i<16;i=i+1) begin
+    a = i;
+    b = i+1;
+    #10;
+  end 
+   $finish;
+end 
+endmodule
