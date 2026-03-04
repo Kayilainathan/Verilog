46d4785a88e072919f03d5d35a6582d8863f2bfa
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-03-04 23:30:51 +0530

    Add 2 to 1 MUX

--- Multiplexer/2 to 1 MUX/src/mux2to1.v
+++ Multiplexer/2 to 1 MUX/src/mux2to1.v
@@ -1,1 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.01.2026 13:08:43
+// Design Name: 
+// Module Name: mux2to1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module mux2to1(
+input a,
+input b,
+input sel,
+output y
+    );
+    assign y = sel?a:b;
+endmodule
