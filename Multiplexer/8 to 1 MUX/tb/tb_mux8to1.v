df62e7b16427ec7239ecbef32fb326e1aeb5801e
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-03-16 22:35:16 +0530

    Add Testbench for the MUX 8 to 1

--- Multiplexer/8 to 1 MUX/tb/tb_mux8to1.v
+++ Multiplexer/8 to 1 MUX/tb/tb_mux8to1.v
@@ -1,1 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.01.2026 16:34:22
+// Design Name: 
+// Module Name: tb_mux8to1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module tb_mux8to1();
+reg [7:0] i;
+reg [2:0] sel;
+wire y;
+reg [11:0] k;
+mux8to1 d0 (i,sel,y);
+initial begin
+i = 8'b11010100;
+for(k=0; k<8; k=k+1) begin
+    sel = k;
+    #10;
+end
+end
+initial begin
+  $monitor("time=%0t sel=%b i=%b y=%b", $time, sel, i, y);
+end
+endmodule
