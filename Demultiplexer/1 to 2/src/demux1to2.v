54c51e35f06dc062e365ad6bc6179295b834e8ed
Kayilainathan <167950279+Kayilainathan@users.noreply.github.com>
2026-03-17 22:49:04 +0530

    Add source file for 1 to 2 Demultiplexer

--- Demultiplexer/1 to 2/src/demux1to2.v
+++ Demultiplexer/1 to 2/src/demux1to2.v
@@ -1,1 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 22.02.2026 22:04:31
+// Design Name: 
+// Module Name: demux1to2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module demux1to2(
+input i,
+input sel,
+output y0,
+output y1
+    );
+    assign y0 = ~sel & i;
+    assign y1 = sel & i;
+endmodule
